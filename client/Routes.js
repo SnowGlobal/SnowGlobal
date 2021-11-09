c82d58a2f324811fa0d26a99baa3805129c575cf
Daniel Schneider <danschneider92@gmail.com>
2021-11-09 12:58:15 -0500

    Edited the boilerplate routes component to show the landing page, created skeleton landing page

--- client/Routes.js
+++ client/Routes.js
@@ -1,59 +1,55 @@
-import React, {Component, Fragment} from 'react'
-import {connect} from 'react-redux'
-import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
-import { Login, Signup } from './components/AuthForm';
-import Home from './components/Home';
-import {me} from './store'
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Login, Signup } from "./components/AuthForm";
+import LandingPage from "./components/LandingPage";
+import { me } from "./store";
 
 /**
  * COMPONENT
  */
-class Routes extends Component {
-  componentDidMount() {
-    this.props.loadInitialData()
-  }
+const Routes = () => {
+  // componentDidMount() {
+  //   this.props.loadInitialData();
+  // }
 
-  render() {
-    const {isLoggedIn} = this.props
-
-    return (
-      <div>
-        {isLoggedIn ? (
-          <Switch>
-            <Route path="/home" component={Home} />
-            <Redirect to="/home" />
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path='/' exact component={ Login } />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-          </Switch>
-        )}
-      </div>
-    )
-  }
-}
+  // render() {
+  // const { isLoggedIn } = this.props;
+  return (
+    <div>
+      <Switch>
+        {/* Routes placed here are available to all visitors */}
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        {/* Displays our Login component as a fallback */}
+        <Redirect to="/" />
+      </Switch>
+    </div>
+  );
+};
 
 /**
  * CONTAINER
  */
-const mapState = state => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id
-  }
-}
+// const mapState = state => {
+//   return {
+//     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
+//     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
+//     isLoggedIn: !!state.auth.id,
+//   };
+// };
 
-const mapDispatch = dispatch => {
-  return {
-    loadInitialData() {
-      dispatch(me())
-    }
-  }
-}
+// const mapDispatch = dispatch => {
+//   return {
+//     loadInitialData() {
+//       dispatch(me());
+//     },
+//   };
+// };
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
+// export default withRouter(connect(mapState, mapDispatch)(Routes));
+
+export default withRouter(Routes);
