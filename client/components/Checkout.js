ca37d3f865c26141dda2b6908db890b684a5d5ce
bsullit <brian.sullivan.ic@gmail.com>
2021-11-10 09:25:34 -0500

    added delete route in api/products

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 
 ///this is not explicitly laid out yet, but presuming
 ///the checkout component is being passed several product objects
-//the component should bring you to a payment and shipping page with forms
+//the component should bring you to a confirmation page
 //on submit remove items from db
 
 export class Checkout extends React.Component {
@@ -17,10 +17,16 @@ export class Checkout extends React.Component {
       <div>
       <h1>Checkout</h1>
       <form id ="payment-form" onSubmit={this.handleSubmit}>
-
-
+        <button type="submit">Confirm Checkout</button>
         </form>
       </div>
     )
   }
 }
+
+const mapDispatch = (dispatch, { history }) => ({
+  //needs redux/ api routes
+  //deleteProduct : (id) => dispatch(deleteProduct(id))
+})
+
+export default connect(null,mapDispatch)(Checkout)
--- server/api/products.js
+++ server/api/products.js
@@ -10,4 +10,20 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.delete(":/id", async (req, res, next) => {
+  try {
+    const removeProduct = await Products.findOne({
+      where: { id: req.params.id },
+    });
+    if (!removeProduct) {
+      Error.status = 404;
+    } else {
+      await removeProduct.destroy();
+      res.sendStatus(204);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
