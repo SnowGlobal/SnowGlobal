eb1806e92a1b592bfc5685f0148d538b365add76
Victorywastaken <rlatmdfl92@gmail.com>
2021-11-11 13:06:58 -0500

    created PK for Cart db

--- server/db/models/Cart.js
+++ server/db/models/Cart.js
@@ -2,6 +2,11 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 module.exports = db.define('Cart', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
   Quantity: {
     type: Sequelize.INTEGER,
     allowNull: false,
